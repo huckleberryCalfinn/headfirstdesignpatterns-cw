34630cbe9c426d2a66494445ee0236224b46e78a
Calvin Witt <54969896+huckleberryCalfinn@users.noreply.github.com>
2022-10-22 20:23:35 -0400

    add heartmodel

---
+++ src/main/java/com/huckleberrycalfinn/mvc/HeartBeatModelAdapter.java
@@ -0,0 +1,64 @@
+package com.huckleberrycalfinn.mvc;
+
+public class HeartBeatModelAdapter implements BeatModelInterface {
+  private HeartModel heartModel;
+
+  public HeartBeatModelAdapter(HeartModel heartModel){
+	this.heartModel = heartModel;
+  }
+
+  @Override
+  public void initialize() {
+	System.out.println("heart model initializes during construction");
+  }
+
+  @Override
+  public void on() {
+	System.out.println("heart model always running");
+  }
+
+  @Override
+  public void off() {
+	System.out.println("can't stop heart model");
+  }
+
+  @Override
+  public void setBPM(int bpm) {
+	System.out.println("heart beats randomly, out of your control");
+  }
+
+  @Override
+  public int getBPM() {
+	return this.heartModel.getHeartRat();
+  }
+
+  @Override
+  public void registerObserver(BeatObserver o) {
+  	this.heartModel.registerObserver(o);
+  }
+
+  @Override
+  public void registerObserver(BPMUpdateObserver o) {
+  	this.heartModel.registerObserver(o);
+  }
+
+  @Override
+  public void removeObserver(BeatObserver o) {
+  	this.heartModel.removeObserver(o);
+  }
+
+  @Override
+  public void removeObserver(BPMUpdateObserver o) {
+  	this.heartModel.removeObserver(o);
+  }
+
+  @Override
+  public void notifyBeatObservers() {
+  	this.heartModel.notifyBeatObservers();
+  }
+
+  @Override
+  public void notifyBPMUpdateObservers() {
+	this.heartModel.notifyBPMUpdateObservers();
+  }
+}
